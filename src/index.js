22dd6dd66e2538d6798587c7fe22c8784a38a1f2
natali-runner <ergodecor@email.cz>
2023-02-11 15:39:24 +0700

    feat: the task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+
+// without reverse()
+
+// const absNum = Math.abs(n);
+// let str = String(absNum).split('').join('');
+
+// return (str.charAt(2) < 1? "" : str.charAt(2)) + str.charAt(1) + str.charAt(0);
+
+// with reverse()
+
+return Math.abs(n).toString().split('').reverse().join('');
+
+};
+
+
